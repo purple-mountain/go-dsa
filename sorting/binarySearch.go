fb9d59da6acefd2cfd8ed973ce76a574f8c67e91
purple-mountain <lmwrong92@gmail.com>
2024-03-29 19:03:12 +0500

    Converted return type to boolean

--- sorting/binarySearch.go
+++ sorting/binarySearch.go
@@ -1,18 +1,18 @@
 package main
 
-func binarySearch(arr []int, haystack int) (int, bool) {
+func binarySearch(arr []int, haystack int) bool {
 	low := 0
 	high := len(arr)
 
 	for high > low {
 		mid := (high + low) / 2
 		if haystack == mid {
-			return mid, true
+			return true
 		} else if haystack > mid {
 			low = mid + 1
 		} else {
 			high = mid
 		}
 	}
-	return -1, false
+	return false
 }
